ff1092869731c56bd5a0d2d4a77932ceba328778
Cocconaki <rekellemail5@gmail.com>
2025-10-06 20:46:17 +0200

    Created methods to borrow and return books

--- User.py
+++ User.py
@@ -13,5 +13,20 @@ class User:
         self.phone = phone
         self.email = email
 
+    def borrow_book(self, book_id, cursor):
+        cursor.execute("INSERT INTO borrowed_books (user_id, book_id) VALUES (?, ?)", (self.id, book_id))
+
+    def return_book(self, book_id, cursor):
+        cursor.execute("DELETE FROM borrowed_books WHERE user_id = ? AND book_id = ?", (self.id, book_id))
+    
+    def show_books(self, cursor):
+        cursor.execute("""
+        SELECT books.id, books.title
+        FROM books
+        JOIN borrowed_books 
+        ON books.id = borrowed_books.book_id 
+        WHERE borrowed_books.user_id = ?""", (self.id,))
+        return cursor.fetchall()
+
 
         
