6eaf673612ac41eda7acbe16992cc009255bbf44
Cocconaki <rekellemail5@gmail.com>
2025-10-06 20:45:54 +0200

    Experienced

--- Main.py
+++ Main.py
@@ -1,4 +1,16 @@
+import time
 from Flow import *
+from ManageDatabase import *
+from Flow import ControlFlow
+from User import User
 
-ControlFlow.add_book()
-ControlFlow.show_all_books()
+with DatabaseConnection() as db:
+    db.init_database()
+    db.connection.commit()
+    manager = ControlFlow(db)
+
+
+    print("Your going to add a book: ")
+    time.sleep(2)
+    manager.add_book()
+    manager.show_all_books()
