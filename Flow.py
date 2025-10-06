c18f1b941a52b853aff5378ad8778cedfc65862b
Cocconaki <rekellemail5@gmail.com>
2025-10-06 20:46:44 +0200

    Polished context manager

--- Flow.py
+++ Flow.py
@@ -1,11 +1,12 @@
 from ManageDatabase import *
 
 
-
-
 class ControlFlow:
+
+    def __init__(self, db_connector):
+        self.db_connector = db_connector
     
-    def add_book():
+    def add_book(self):
         
         name = str(input("Enter books name: "))
         number_of_pages = int(input("Enter the number of pages: "))
@@ -15,20 +16,22 @@ class ControlFlow:
         is_avalable = str(input("Enter if the book is on stock (y/n?): ")).strip().lower() == "y"
         count = int(input("Enter how many is on stock at the moment: "))
         
-        cursor.execute("INSERT INTO books (name, number_of_pages, author, category, title, is_available, count) VALUES (?, ?, ?, ?, ?, ?, ?)", (name, number_of_pages, author, category, title, is_avalable, count ))
-
-
-    def delete_book():
+        self.db_connector.cursor.execute("INSERT INTO books (name, number_of_pages, author, category, title, is_available, count) VALUES (?, ?, ?, ?, ?, ?, ?)", (name, number_of_pages, author, category, title, is_avalable, count ))
+        self.db_connector.connection.commit()
+    
+    def delete_book(self):
         id_to_del = int(input("Enter books ID to delete: "))
-        cursor.execute("DELETE FROM books WHERE id = ?", (id_to_del,))
+        self.db_connector.cursor.execute("DELETE FROM books WHERE id = ?", (id_to_del,))
+        self.db_connector.connection.commit()
 
-    def show_all_books():
+    def show_all_books(self):
         
-        cursor.execute("SELECT * FROM books")
-        rows = cursor.fetchall()
+        self.db_connector.cursor.execute("SELECT * FROM books")
+        rows = self.db_connector.cursor.fetchall()
 
         for row in rows:
             print(row)
     
-    def delete_all():
-        cursor.execute("DELETE FROM books")
+    def delete_all(self):
+        self.db_connector.cursor.execute("DELETE FROM books")
+        self.db_connector.connection.commit()
